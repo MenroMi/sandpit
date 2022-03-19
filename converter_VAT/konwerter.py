669ae703ae33f74455091de15e723faf86ecf479
MenroMi <folkbread@gmail.com>
2022-03-19 19:27:34 +0100

    Poprawki kodu

--- converter_VAT/konwerter.py
+++ converter_VAT/konwerter.py
@@ -1,51 +1,41 @@
-# zadanie: zrobić konwerter VAT brutto na netto/netto na brutto
+def summary_brutto(sum_choose, choose_vat):
+    take_brutto = sum_choose * (choose_vat / 100)
 
-# 1. Komputer ma prosić o wybór procentu podatku
-# 2. Komputer ma prosić o konwertację brutto na netto lub odwrotnie
-# 3. Komputer ma zapytać o kwotę brutto lub netto
+    return take_brutto
 
-# Pierwszy krok: napisać funkcję dla wyboru brutto > netto czy brutto < netto
-# def summary_brutto(take_brutto):
-#     sum_choose = int(input())
-#     take_brutto_or_netto = str(input())
-#     if take_brutto_or_netto == take_brutto:
-#             take_brutto = sum_choose * (23 / 100)
-#             return print("Sum brutto", take_brutto)
+def summary_netto(sum_choose_1, choose_vat_1):
+    vat_kwoty_brutto = (sum_choose_1 * choose_vat_1)//(100 + choose_vat_1)
+    sum_netto = sum_choose_1 - vat_kwoty_brutto
 
-sum_choose = int(input()) #podajemy kwotę netto/brutto
-take_brutto_or_netto = str(input()) # podajemy brutto or netto
-choose_vat = int(input()) # podajemy procent obliczania
+    return sum_netto, vat_kwoty_brutto
 
-if choose_vat == 23:
-    def summary_brutto():
-        if take_brutto_or_netto == "brutto" or "BRUTTO":
-                take_brutto = sum_choose * (choose_vat / 100)
-            # print("Sum brutto", take_brutto)
-        return take_brutto
-    wynik = summary_brutto()
-    print("Sum brutto", wynik)
-elif choose_vat == 0:
-    def summary_brutto():
-        if take_brutto_or_netto == "brutto":
-                take_brutto = sum_choose * (choose_vat / 100)
-            # print("Sum brutto", take_brutto)
-        return take_brutto
-    wynik = summary_brutto()
-    print("Sum brutto", wynik)
-elif choose_vat == 5:
-    def summary_brutto():
-        if take_brutto_or_netto == "brutto":
-                take_brutto = sum_choose * (choose_vat / 100)
-        return take_brutto
-    wynik = summary_brutto()
-    print("Sum brutto", wynik)
-elif choose_vat == 8:
-    def summary_brutto():
-        if take_brutto_or_netto == "brutto":
-                take_brutto = sum_choose * (choose_vat / 100)
-            # print("Sum brutto", take_brutto)
-        return take_brutto
-    wynik = summary_brutto()
-    print("Sum brutto", wynik)
-else:
-    print('Negative. Please give another procent VAT')
+if __name__ == "__main__": # odpala poniższy kod
+
+    take_brutto_or_netto = str(input('Sposób obliczania (brutto/netto, netto/brutto): ')) # podajemy brutto or netto
+
+    sposob_obliczania = take_brutto_or_netto
+
+    while True:
+        sum_choose = input(f'Podaj kwotę {sposob_obliczania}:').strip() #podajemy kwotę netto/brutto
+        if "." in sum_choose:
+            sum_choose = float(sum_choose)
+            break
+        if sum_choose.isdigit(): # czy wszystkie znaki stringa są cyfraami
+            sum_choose = int(sum_choose)
+            break
+        else:
+            print('Podaj cyferki!')
+            continue
+
+    choose_vat = int(input('Podaj procent VAT: ')) # podajemy procent obliczania
+
+    if take_brutto_or_netto == "netto":
+        n = summary_netto(sum_choose, choose_vat)
+        vat_brutto = n[1]
+        kwota_netto = n[0]
+        print(f"\nVAT od kwoty brutto: {vat_brutto} zł")
+        print(f"Sum netto z {choose_vat}%:", kwota_netto, "zł")
+
+    if take_brutto_or_netto == "brutto":
+        b = summary_brutto(sum_choose, choose_vat)
+        print(f"Sum brutto z {choose_vat}%:", b)
